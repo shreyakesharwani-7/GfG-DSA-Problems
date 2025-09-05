5c1b7d0f72432c35bb85355a68afead5ca20f5d7
Shreya Kesharwani <shreyakesharwani5524@gmail.com>
2025-09-05 22:46:13 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Reverse digits/reverse-digits.java
@@ -0,0 +1,16 @@
+// User function Template for Java
+
+class Solution {
+    public int reverseDigits(int n) {
+        // Code here
+        int rev = 0;
+       
+        while(n != 0)
+        {
+            int rem = n % 10;
+            rev = rev * 10 + rem;
+            n = n / 10;
+        }
+        return rev;
+    }
+}
