03240cf2eac81479b7cadec4247c4ad79d6759c4
Shreya Kesharwani <shreyakesharwani5524@gmail.com>
2026-04-02 21:08:56 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Longest Bitonic subsequence/longest-bitonic-subsequence.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int longestBitonicSequence(int n, int[] nums) {
+        int[] lis = new int[n];
+        int[] lds = new int[n];
+        Arrays.fill(lis, 1);
+        Arrays.fill(lds, 1);
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < i; j++) {
+                if (nums[j] < nums[i]) {
+                    lis[i] = Math.max(lis[i], lis[j] + 1);
+                }
+            }
+        }
+        for (int i = n - 1; i >= 0; i--) {
+            for (int j = n - 1; j > i; j--) {
+                if (nums[j] < nums[i]) {
+                    lds[i] = Math.max(lds[i], lds[j] + 1);
+                }
+            }
+        }
+        int max = 0;
+
+        for (int i = 0; i < n; i++) {
+            if (lis[i] > 1 && lds[i] > 1) 
+                max = Math.max(max, lis[i] + lds[i] - 1);
+        }
+        return max;
+    }
+}
